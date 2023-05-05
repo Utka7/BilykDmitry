cf9d06cbcab4250ed13e92abbd687b413ea79eba
Your Name <dima.bilyk.02@mail.ru>
2023-05-05 14:04:03 +0300

    middle-1: solution

---
+++ middle/middle-1/src/Searcher.java
@@ -0,0 +1,20 @@
+public class Searcher {
+    public int binarySearch(int[] array, int key){
+        var start = 0;
+        var end = array.length - 1;
+
+        while (start <= end){
+            int middle = (start + end) >> 1;
+            if(key == array[middle]){
+                return middle;
+            }
+            else if(key < array[middle]){
+                end = middle - 1;
+            }
+            else{
+                start = middle + 1;
+            }
+        }
+        return -1;
+    }
+}
