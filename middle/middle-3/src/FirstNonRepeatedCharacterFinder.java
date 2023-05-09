3e119611428fd38685688e64533b081e1575340d
Your Name <dima.bilyk.02@mail.ru>
2023-05-09 12:22:54 +0300

    middle-3: second solution

--- middle/middle-3/src/FirstNonRepeatedCharacterFinder.java
+++ middle/middle-3/src/FirstNonRepeatedCharacterFinder.java
@@ -1,23 +1,35 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.HashMap;
 
 public class FirstNonRepeatedCharacterFinder {
     public char find(String string){
+        var charCountMap = new HashMap<Character, Integer>();
 
-        char resultCharacter = ' ';
-        var charCountMap = new LinkedHashMap<Character, Integer>();
-
-        for (int i = 0; i < string.length(); i++){
-            var ch = string.charAt(i);
+        for (char ch : string.toCharArray()){
             charCountMap.put(ch, charCountMap.getOrDefault(ch,0 ) + 1);
         }
 
-        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()){
-            if (entry.getValue() == 1){
-                return entry.getKey();
+        for (char ch : string.toCharArray()) {
+            if (charCountMap.get(ch) == 1) {
+                return ch;
             }
         }
 
         throw new RuntimeException("a non-repeatable character was not found in the string");
+    }
+
+    public int find(int[] array){
+        var numberCountMap = new HashMap<Integer, Integer>();
+
+        for (int number : array){
+            numberCountMap.put(number, numberCountMap.getOrDefault(number, 0) + 1);
+        }
+
+        for (int number : array){
+            if (numberCountMap.get(number) == 1){
+                return number;
+            }
+        }
+
+        throw new RuntimeException("a non-repeatable character was not found in the array");
     }
 }
--- middle/middle-3/src/Main.java
+++ middle/middle-3/src/Main.java
@@ -1,9 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
         String string = "hello";
+        int[] array = {1,1,2,3,5,6,4};
         var firstNonRepeatedCharacterFinder = new FirstNonRepeatedCharacterFinder();
         var nonRepeatCharacter = firstNonRepeatedCharacterFinder.find(string);
+        var nonRepeatNumber = firstNonRepeatedCharacterFinder.find(array);
         System.out.println(nonRepeatCharacter);
+        System.out.println(nonRepeatNumber);
     }
 }
