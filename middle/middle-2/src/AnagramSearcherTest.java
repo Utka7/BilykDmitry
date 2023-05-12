d97a55af254bf060b5ccd5389c5c5a35e230c85a
Your Name <dima.bilyk.02@mail.ru>
2023-05-12 12:53:49 +0300

    middle-2: rename test

--- middle/middle-2/src/AnagramSearcherTest.java
+++ middle/middle-2/src/AnagramSearcherTest.java
@@ -1,34 +1,25 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AnagramSearcherTest {
 
-    @org.junit.jupiter.api.Test
-    void isAnagram() {
+    @Test
+    void isAnagram_ValidInput_ReturnsTrue() {
         var searcher = new AnagramSearcher();
         var firstString = "race";
         var secondString = "care";
         var result = searcher.isAnagram(firstString, secondString);
         assertTrue(result);
+    }
 
-        firstString = "car";
-        secondString = "care";
-        result = searcher.isAnagram(firstString, secondString);
+    @Test
+    void isAnagram_InvalidInput_ReturnsFalse() {
+        var searcher = new AnagramSearcher();
+        var firstString = "car";
+        var secondString = "care";
+        var result = searcher.isAnagram(firstString, secondString);
         assertFalse(result);
-
-        firstString = "listen";
-        secondString = "silent";
-        result = searcher.isAnagram(firstString, secondString);
-        assertTrue(result);
-
-        firstString = "hello";
-        secondString = "oelhl";
-        result = searcher.isAnagram(firstString, secondString);
-        assertTrue(result);
-
-        firstString = "qwerty";
-        secondString = "ytrewq";
-        result = searcher.isAnagram(firstString, secondString);
-        assertTrue(result);
     }
 }
