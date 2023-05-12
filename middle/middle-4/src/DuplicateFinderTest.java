6d2fa035b5672c9cdd78e067f6a0326be2265bec
Your Name <dima.bilyk.02@mail.ru>
2023-05-12 13:10:02 +0300

    middle-4: rename test

--- middle/middle-4/src/DuplicateFinderTest.java
+++ middle/middle-4/src/DuplicateFinderTest.java
@@ -1,3 +1,5 @@
+import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 import java.util.HashMap;
 
@@ -5,24 +7,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DuplicateFinderTest {
 
-    @org.junit.jupiter.api.Test
-    void findDuplicate_listInteger_() {
+    @Test
+    void findDuplicate_ListInteger_ReturnExpectedResult() {
         var duplicateFinderInteger = new DuplicateFinder<Integer>();
         var listInteger = Arrays.asList(1, 3, 5, 1, 2, 3, 5, 7, 5);
-        var expectedResultInteger = new HashMap<Integer,Integer>();
-        expectedResultInteger.put(1,2);
-        expectedResultInteger.put(3,2);
-        expectedResultInteger.put(5,3);
+        var expectedResultInteger = new HashMap<Integer, Integer>();
+        expectedResultInteger.put(1, 2);
+        expectedResultInteger.put(3, 2);
+        expectedResultInteger.put(5, 3);
         var resultInteger = duplicateFinderInteger.findDuplicate(listInteger);
         assertEquals(expectedResultInteger, resultInteger);
-
+    }
+    @Test
+    void findDuplicate_ListString_ReturnExpectedResult(){
         var duplicateFinderString = new DuplicateFinder<String>();
         var listString = Arrays.asList("test", "sqrt", "null", "test", "int");
         var expectedResultString = new HashMap<String,Integer>();
         expectedResultString.put("test",2);
         var resultString = duplicateFinderString.findDuplicate(listString);
         assertEquals(expectedResultString,resultString);
+    }
 
+    @Test
+    void findDuplicate_ListWithoutDuplicates_ReturnExpectedResult(){
         var duplicateFinderChar = new DuplicateFinder<Character>();
         var listChar = Arrays.asList('q','w','e','r');
         var expectedResultChar = new HashMap<Character,Integer>();
