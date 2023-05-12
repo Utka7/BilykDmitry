8b4ef9151d3dd363a1c7bf315bc9f5f24643c64c
Your Name <dima.bilyk.02@mail.ru>
2023-05-12 13:05:55 +0300

    middle-3: rename test

--- middle/middle-3/src/FirstNonRepeatedCharacterFinderTest.java
+++ middle/middle-3/src/FirstNonRepeatedCharacterFinderTest.java
@@ -1,13 +1,25 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class FirstNonRepeatedCharacterFinderTest {
 
-    @org.junit.jupiter.api.Test
-    void find() {
+    @Test
+    void find_NonRepeatedCharacter_ReturnExpectedResult() {
         var finder = new FirstNonRepeatedCharacterFinder();
         assertEquals('l', finder.find("alphabet"));
-        assertEquals('e', finder.find("hhello"));
+    }
+
+    @Test
+    void find_RepeatedCharacter_ThrowRuntimeException(){
+        var finder = new FirstNonRepeatedCharacterFinder();
         assertThrows(RuntimeException.class, () -> finder.find("qqqqqqqqqq"));
+        assertThrows(RuntimeException.class, () -> finder.find(""));
+    }
+
+    @Test
+    void find_EmptyString_ThrowRuntimeException(){
+        var finder = new FirstNonRepeatedCharacterFinder();
         assertThrows(RuntimeException.class, () -> finder.find(""));
     }
 }
--- middle/middle-4/src/DuplicateFinderTest.java
+++ middle/middle-4/src/DuplicateFinderTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class DuplicateFinderTest {
 
     @org.junit.jupiter.api.Test
-    void findDuplicate() {
+    void findDuplicate_listInteger_() {
         var duplicateFinderInteger = new DuplicateFinder<Integer>();
         var listInteger = Arrays.asList(1, 3, 5, 1, 2, 3, 5, 7, 5);
         var expectedResultInteger = new HashMap<Integer,Integer>();
