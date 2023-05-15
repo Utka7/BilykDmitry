fc5c09f644c14390936a2316f71ec44fd4c19686
Your Name <dima.bilyk.02@mail.ru>
2023-05-15 11:44:42 +0300

    high-1: corrections in the code

--- high/high-1/src/main/java/org/example/utka/TestSpring.java
+++ high/high-1/src/main/java/org/example/utka/TestSpring.java
@@ -4,24 +4,23 @@ import org.example.utka.annotation.AnnotationBean;
 import org.example.utka.java.AppConfig;
 import org.example.utka.java.JavaBean;
 import org.example.utka.xml.XmlBean;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
+        var contextXml = new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        XmlBean xmlBean = context.getBean("xmlBean", XmlBean.class);
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        var xmlBean = contextXml.getBean("xmlBean", XmlBean.class);
+        var applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
         System.out.println(xmlBean.getName());
 
-        AnnotationBean annotationBean = context.getBean("annotationBean",AnnotationBean.class);
+        var annotationBean = contextXml.getBean("annotationBean", AnnotationBean.class);
         System.out.println(annotationBean.getName());
 
-        JavaBean javaBean = applicationContext.getBean(JavaBean.class);
+        var javaBean = applicationContext.getBean(JavaBean.class);
         System.out.println(javaBean.getName());
 
-        context.close();
+        contextXml.close();
     }
 }
--- high/high-1/src/main/java/org/example/utka/annotation/AnnotationBean.java
+++ high/high-1/src/main/java/org/example/utka/annotation/AnnotationBean.java
@@ -1,6 +1,5 @@
 package org.example.utka.annotation;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component("annotationBean")
