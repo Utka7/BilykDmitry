2812b9f9f2d08e1d6757d3b27f63dd49c9ea3853
Your Name <dima.bilyk.02@mail.ru>
2023-05-17 15:10:48 +0300

    high-2: delete @repository

--- high/high-2/src/main/java/com/example/high2/dao/UserDao.java
+++ high/high-2/src/main/java/com/example/high2/dao/UserDao.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
 import java.util.List;
 
-@Repository
 public class UserDao {
     private final List<UserDto> users = new ArrayList<UserDto>();
 
