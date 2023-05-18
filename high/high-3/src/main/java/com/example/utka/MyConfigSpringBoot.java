ac7a59f0bf47e3ad248ea9a7ac1d1945086161c4
Your Name <dima.bilyk.02@mail.ru>
2023-05-18 14:21:09 +0300

    high-3: del annotation Component

--- high/high-3/src/main/java/com/example/utka/MyConfigSpringBoot.java
+++ high/high-3/src/main/java/com/example/utka/MyConfigSpringBoot.java
@@ -5,15 +5,11 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-//@Component
 @ConfigurationProperties(prefix = "myapp")
 public class MyConfigSpringBoot {
     private String name;
