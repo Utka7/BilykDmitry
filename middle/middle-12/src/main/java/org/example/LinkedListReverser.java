a6dbe98696914c0e364443496ecc0cde3b499401
Your Name <dima.bilyk.02@mail.ru>
2023-05-23 17:16:29 +0300

    middle-12: solution

---
+++ middle/middle-12/src/main/java/org/example/LinkedListReverser.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import java.util.Random;
+
+public class LinkedListReverser {
+
+    public Node reverseLinkedList(Node head) {
+        Node currentNode = head;
+        Node nextNode;
+        Node previousNode = null;
+
+        while (currentNode != null) {
+            nextNode = currentNode.getNext();
+            currentNode.setNext(previousNode);
+            previousNode = currentNode;
+            currentNode = nextNode;
+        }
+
+        return previousNode;
+    }
+
+
+}
---
+++ middle/middle-12/src/test/java/org/example/LinkedListReverserTest.java
@@ -0,0 +1,51 @@
+package org.example;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
+public class LinkedListReverserTest extends TestCase {
+
+    @Test
+    public void testReverseLinkedList() {
+        var linkedList = generateRandomLinkedList();
+
+        LinkedListReverser reverser = new LinkedListReverser();
+
+        linkedList = reverser.reverseLinkedList(linkedList);
+
+        assertReversedLinkedList(linkedList);
+
+    }
+
+    private Node generateRandomLinkedList(){
+        var length = 5;
+        var value = 1;
+        Node head = null;
+        Node prev = null;
+
+        for (var i = 0; i < length; i++) {
+            Node newNode = new Node();
+            newNode.setValue(value++);
+            newNode.setNext(null);
+
+            if (head == null) {
+                head = newNode;
+            } else {
+                prev.setNext(newNode);
+            }
+
+            prev = newNode;
+        }
+
+        return head;
+    }
+
+    private void assertReversedLinkedList(Node head) {
+        assertEquals(5, head.getValue());
+        assertEquals(4, head.getNext().getValue());
+        assertEquals(3, head.getNext().getNext().getValue());
+        assertEquals(2, head.getNext().getNext().getNext().getValue());
+        assertEquals(1, head.getNext().getNext().getNext().getNext().getValue());
+        assertNull(head.getNext().getNext().getNext().getNext().getNext());
+    }
+}
